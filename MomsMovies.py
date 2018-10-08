4073a923c2dd55a6077919ff47ce7a514e576022
David A York <medmatix@gmail.com>
2018-10-08 19:54:16 -0400

    Initial commit

---
+++ MomsMovies.py
@@ -0,0 +1,25 @@
+'''
+MomsMovies.py
+Created on Oct 8, 2018
+@summary: A project to construct and access a Home Movies Database
+@version: 0.01
+@author: David York
+@contact: 
+@copyright: 
+'''
+# ######################
+# imports
+# ######################
+
+class MomsMovies(object):
+    ''' 
+    Main class module for Movies Databaser Application
+    '''
+    def __init__(self):
+        '''
+        Constructor for Mom
+        '''
+        pass
+    
+if __name__ == '__main__':
+    pass
